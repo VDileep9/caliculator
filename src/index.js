fc5b59cefcf6db421ab9caa3c1de72977a16d67c
dileep <valluridileep999@gmail.com>
2022-05-13 12:16:49 +0530

    caliculator

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DigitButton from './components/DigitButton';
+import OperationButton from './components/OperationButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <DigitButton/>
+    <OperationButton/>
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
